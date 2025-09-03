74b6db2a96cec55c05f73e4dfd4d3ae0be850c87
thinhtran-inno <thinh.tran@inno.ai.vn>
2025-09-03 11:51:02 +0700

    Add creating a basic token based on openzeppelin contract

---
+++ Token.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(uint256 initialSupply) ERC20("TestTokenA", "TTA") {
+        _mint(msg.sender, initialSupply * 10**6);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+}
